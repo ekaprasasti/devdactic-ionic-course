3d139d2fdb92c0af094a0608d5dc27edcf7f2c9d
eka prasasti <ekaputraprasasti@gmail.com>
2017-07-03 21:56:31 +0700

    #1 getting started project directory

---
+++ Ionic Course #1 Getting Started/ionicStarWarsApp/src/pages/home/home.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+	myVariable: string = 'The force is with me!';
+
+  constructor(public navCtrl: NavController) {
+
+	}
+
+	updateMyValue() {
+		 this.myVariable = 'Now the force is even stronger!';
+	}
+
+}
